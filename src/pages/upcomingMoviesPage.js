655eacd57175a5f4e8003405dc2ec37be858d418
John Cagney <59435586+JCagney@users.noreply.github.com>
2021-07-03 11:07:15 +0100

    Added Upcoming movies page

---
+++ src/pages/upcomingMoviesPage.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage'
+
+const UpcomingMoviesPage = (props) => {
+    const [movies, setMovies] = useState([]);
+    const favorites = movies.filter(m => m.favorite)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  
+    const addToFavorites = (movieId) => {
+      const updatedMovies = movies.map((m) =>
+        m.id === movieId ? { ...m, favorite: true } : m
+      );
+      setMovies(updatedMovies);
+    };
+  
+    useEffect(() => {
+      getUpcomingMovies()
+        .then((movies) => {
+          setMovies(movies);
+        });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+  
+    return (
+      <PageTemplate
+        title='Upcoming Movies'
+        movies={movies}
+        selectFavorite={addToFavorites}
+      />
+    );
+  };
+
+export default withRouter(UpcomingMoviesPage);
